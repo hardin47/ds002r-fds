88c5b0beb367893f6e8abb054e87a2349e7673e8
hardin47 <jo.hardin@pomona.edu>
2025-04-30 13:40:20 -0700

    r tips

---
+++ Shiny/app.R
@@ -0,0 +1,20 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    https://shiny.posit.co/
+#
+
+library(shiny)
+
+ui <- fluidPage(
+  "Hello, world!"
+)
+
+server <- function(input, output, session) {
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
